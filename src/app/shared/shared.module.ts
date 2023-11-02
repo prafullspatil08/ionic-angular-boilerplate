c50d87fd12ceeba19d8d8c3d252686ac693ab512
prafullspatil <prafull.patil@mindbowser.com>
2023-11-02 12:32:36 +0530

    added utility classes & design cards

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { TranslateModule } from '@ngx-translate/core';
 import { IonicModule } from '@ionic/angular';
+import { ButtonComponent } from './button/button.component';
 
 
 @NgModule({
   declarations: [
+    ButtonComponent
   ],
   imports: [
     CommonModule,
@@ -14,7 +16,8 @@ import { IonicModule } from '@ionic/angular';
   ],
   exports: [
     TranslateModule,
-    IonicModule
+    IonicModule,
+    ButtonComponent
   ]
 })
 export class SharedModule { }
